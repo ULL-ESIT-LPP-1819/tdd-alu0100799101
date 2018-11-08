aaac3cc28d5c0b48d13517e1d40395721ff51d74
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-08 01:11:08 +0000

    Prueba de Azucares

--- spec/nutricion_spec.rb
+++ spec/nutricion_spec.rb
@@ -4,8 +4,8 @@ require "/home/ubuntu/workspace/LPP-Practica2/tdd-alu0100799101/gema/lib/gema/nu
 describe Nutricion do
     
     before :each do
-        @etiqueta1 = Nutricion.new("Zumo",1,0,4.7)
-        @etiqueta2 = Nutricion.new("Arroz",0.7,0.2,77)
+        @etiqueta1 = Nutricion.new("Zumo",1,0,4.7,4.2)
+        @etiqueta2 = Nutricion.new("Arroz",0.7,0.2,77,0.5)
     end
     
     describe"Pruebas practica 6" do
@@ -24,6 +24,10 @@ describe Nutricion do
         it"Prueba Hidratos" do
             expect(@etiqueta1.hidratos).to eq(4.7)
             expect(@etiqueta2.hidratos).to eq(77)
+        end
+         it"Azucares" do
+            expect(@etiqueta1.azucares).to eq(4.2)
+            expect(@etiqueta2.azucares).to eq(0.5)
         end
     end
 end
