b2752cf58e03c56df5a0ec22dbdd1276990a5257
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-08 01:13:44 +0000

    Añadida la variable azucares

--- lib/gema/nutricion.rb
+++ lib/gema/nutricion.rb
@@ -1,10 +1,11 @@
 class Nutricion 
-    attr_reader :nombre , :grasa ,:saturadas,:hidratos
+    attr_reader :nombre , :grasa ,:saturadas,:hidratos,:azucares
     
-    def initialize(nombre,grasa,saturadas,hidratos)
+    def initialize(nombre,grasa,saturadas,hidratos,azucares)
     @nombre = nombre
 	@grasa = grasa
 	@saturadas=saturadas
 	@hidratos=hidratos
+	@azucares=azucares
     end
 end
