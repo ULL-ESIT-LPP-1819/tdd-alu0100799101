30fc9bca3ec199900f0ec60e3b3e47e910981cc8
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-08 00:50:35 +0000

    Añadimos nueva variable instancia etiqueta2 y una nueva prueba grasas saturadas

--- spec/nutricion_spec.rb
+++ spec/nutricion_spec.rb
@@ -4,16 +4,22 @@ require "/home/ubuntu/workspace/LPP-Practica2/tdd-alu0100799101/gema/lib/gema/nu
 describe Nutricion do
     
     before :each do
-        @etiqueta1 = Nutricion.new("Zumo",1)
-        
+        @etiqueta1 = Nutricion.new("Zumo",1,0)
+        @etiqueta2 = Nutricion.new("Arroz",0.7,0.2)
     end
     
     describe"Pruebas practica 6" do
         it "Prueba nombre" do
             expect(@etiqueta1.nombre).to eq('Zumo')
+             expect(@etiqueta2.nombre).to eq('Arroz')
         end
         it"Prueba Grasa" do
             expect(@etiqueta1.grasa).to eq(1)
+             expect(@etiqueta2.grasa).to eq(0.7)    
         end    
+        it"Prueba Grasa Saturada" do
+            expect(@etiqueta1.saturada).to eq(0)
+            expect(@etiqueta2.saturada).to eq(0.2)
+        end
     end
 end
