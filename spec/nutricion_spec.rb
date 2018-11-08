45a4346cf8f3987d575e0be3c65af23772da396b
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-08 00:59:51 +0000

    Nueva prueba con los hidratos

--- spec/nutricion_spec.rb
+++ spec/nutricion_spec.rb
@@ -4,8 +4,8 @@ require "/home/ubuntu/workspace/LPP-Practica2/tdd-alu0100799101/gema/lib/gema/nu
 describe Nutricion do
     
     before :each do
-        @etiqueta1 = Nutricion.new("Zumo",1,0)
-        @etiqueta2 = Nutricion.new("Arroz",0.7,0.2)
+        @etiqueta1 = Nutricion.new("Zumo",1,0,4.7)
+        @etiqueta2 = Nutricion.new("Arroz",0.7,0.2,77)
     end
     
     describe"Pruebas practica 6" do
@@ -20,6 +20,10 @@ describe Nutricion do
         it"Prueba Grasa Saturada" do
             expect(@etiqueta1.saturadas).to eq(0)
             expect(@etiqueta2.saturadas).to eq(0.2)
+        end
+        it"Prueba Hidratos" do
+            expect(@etiqueta1.hidratos).to eq(4.7)
+            expect(@etiqueta2.hidratos).to eq(77)
         end
     end
 end
