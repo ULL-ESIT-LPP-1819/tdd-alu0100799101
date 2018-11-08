e381b07bf384c211e185b60be806fa6bb2fcdc92
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-08 01:20:08 +0000

    Añadiendo la variable proteinas

--- lib/gema/nutricion.rb
+++ lib/gema/nutricion.rb
@@ -1,11 +1,12 @@
 class Nutricion 
-    attr_reader :nombre , :grasa ,:saturadas,:hidratos,:azucares
+    attr_reader :nombre , :grasa ,:saturadas,:hidratos,:azucares,:proteinas
     
-    def initialize(nombre,grasa,saturadas,hidratos,azucares)
+    def initialize(nombre,grasa,saturadas,hidratos,azucares,proteinas)
     @nombre = nombre
 	@grasa = grasa
 	@saturadas=saturadas
 	@hidratos=hidratos
 	@azucares=azucares
+	@proteinas=proteinas
     end
 end
