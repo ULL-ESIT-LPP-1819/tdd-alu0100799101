009a399307c61a1b851b258e25e4f79a230dca33
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-08 01:24:15 +0000

    Añadimos la variable sal para que funcione nuestra prueba2

--- lib/gema/nutricion.rb
+++ lib/gema/nutricion.rb
@@ -1,12 +1,13 @@
 class Nutricion 
-    attr_reader :nombre , :grasa ,:saturadas,:hidratos,:azucares,:proteinas
+    attr_reader :nombre , :grasa ,:saturadas,:hidratos,:azucares,:proteinas,:sal
     
-    def initialize(nombre,grasa,saturadas,hidratos,azucares,proteinas)
+    def initialize(nombre,grasa,saturadas,hidratos,azucares,proteinas,sal)
     @nombre = nombre
 	@grasa = grasa
 	@saturadas=saturadas
 	@hidratos=hidratos
 	@azucares=azucares
 	@proteinas=proteinas
+	@sal=sal
     end
 end
