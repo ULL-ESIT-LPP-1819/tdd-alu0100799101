5a217ed0fe36b1fec97719200f2e7045b2c4214e
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-08 13:49:52 +0000

    Nuevas Pruebas para Almidon polialcohol y fibraalimenticia

--- spec/nutricion_spec.rb
+++ spec/nutricion_spec.rb
@@ -4,8 +4,8 @@ require "/home/ubuntu/workspace/LPP-Practica2/tdd-alu0100799101/gema/lib/gema/nu
 describe Nutricion do
     
     before :each do
-        @etiqueta1 = Nutricion.new("Zumo",1,0,0,0,4.7,4.2,0.5,0.01)
-        @etiqueta2 = Nutricion.new("Arroz",0.7,0.2,0,0,77,0.5,7.2,0.01)
+        @etiqueta1 = Nutricion.new("Zumo",1,0,0,0,4.7,4.2,0,0,0,0.5,0.01)
+        @etiqueta2 = Nutricion.new("Arroz",0.7,0.2,0,0,77,0.5,0,0,0,7.2,0.01)
     end
     
     describe"Pruebas practica 6" do
@@ -36,6 +36,18 @@ describe Nutricion do
          it"Pruebas Azucares" do
             expect(@etiqueta1.azucares).to eq(4.2)
             expect(@etiqueta2.azucares).to eq(0.5)
+        end
+        it"Pruebas Polialcohol" do
+            expect(@etiqueta1.polial).to eq(0)
+            expect(@etiqueta2.polial).to eq(0)
+        end
+        it"Pruebas Almidon" do
+            expect(@etiqueta1.almidon).to eq(0)
+            expect(@etiqueta2.almidon).to eq(0)
+        end
+        it"Pruebas Fibra" do
+            expect(@etiqueta1.fibra).to eq(0)
+            expect(@etiqueta2.fibra).to eq(0)
         end
         it"Pruebas Proteinas" do
             expect(@etiqueta1.proteinas).to eq(0.5)
