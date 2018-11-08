6c7a8481e742133b42f7a743f9141ce39226f430
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-08 10:58:41 +0000

    Añadido en codigo fuente los polinsaturadas

--- lib/gema/nutricion.rb
+++ lib/gema/nutricion.rb
@@ -1,11 +1,12 @@
 class Nutricion 
-    attr_reader :nombre , :grasa ,:saturadas,:mono,:hidratos,:azucares,:proteinas,:sal
+    attr_reader :nombre , :grasa ,:saturadas,:mono,:polin,:hidratos,:azucares,:proteinas,:sal
     
-    def initialize(nombre,grasa,saturadas,mono,hidratos,azucares,proteinas,sal)
+    def initialize(nombre,grasa,saturadas,mono,polin,hidratos,azucares,proteinas,sal)
     @nombre = nombre
 	@grasa = grasa
 	@saturadas=saturadas
 	@mono=mono
+	@polin=polin
 	@hidratos=hidratos
 	@azucares=azucares
 	@proteinas=proteinas
