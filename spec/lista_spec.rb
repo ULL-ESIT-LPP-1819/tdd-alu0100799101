62dca3998c423b06090221998d3ad1da0708b6ff
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-15 19:26:29 +0000

    Prueba para ver si se puede insertar un elemento al principio y final de la lista

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -1,26 +1,37 @@
 require 'spec_helper'
 
-RSpec.describe Gema::Nodo do
-    before :all do
-        @mi_nodo = Gema::Nodo.new(2,nil,nil)
-    end
-
-    describe "Atributos del nodo" do
-        it "El nodo contiene los atributos datos, siguiente y previo" do
-            expect(@mi_nodo).to have_attributes(:valor=>2)
-            expect(@mi_nodo).to have_attributes(:siguiente=>nil)
-            expect(@mi_nodo).to have_attributes(:previo=>nil)
+    RSpec.describe Gema::Nodo do
+        before :all do
+            @mi_nodo = Gema::Nodo.new(2,nil,nil)
         end
-    end
-end    
-RSpec.describe Gema::Lista do
-    before :each do
-        @mi_lista = Gema::Lista.new()
-    end
-     describe "#Atributos de la lista" do
-        it "La lista tiene cabeza y cola" do
-            expect(@mi_lista).to have_attributes(:cabeza=>nil)
-            expect(@mi_lista).to have_attributes(:cola=>nil)
+    
+        describe "Atributos del nodo" do
+            it "El nodo contiene los atributos datos, siguiente y previo" do
+                expect(@mi_nodo).to have_attributes(:valor=>2)
+                expect(@mi_nodo).to have_attributes(:siguiente=>nil)
+                expect(@mi_nodo).to have_attributes(:previo=>nil)
+            end
         end
+    end    
+    RSpec.describe Gema::Lista do
+        before :each do
+            @mi_lista = Gema::Lista.new()
+        end
+             describe "#Atributos de la lista" do
+                it "La lista tiene cabeza y cola" do
+                    expect(@mi_lista).to have_attributes(:cabeza=>nil)
+                    expect(@mi_lista).to have_attributes(:cola=>nil)
+                end
+             end
+             describe "#Funcionalidad de la lista" do
+                it "Se puede insertar un elemento al principio y final de la lista" do
+                    expect(@mi_lista).to respond_to (:push_back)
+                    @mi_lista.push_back(2003)
+                    expect(@mi_lista.back).to eq(2003)
+                    expect(@mi_lista).to respond_to (:push_front)
+                    @mi_lista.push_front('Turko')
+                    expect(@mi_lista.front).to eq('Turko')
+                end
+            end
+        
     end
-end
