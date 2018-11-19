d629a8da0538b273280fdf02fc487be8c521ea77
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-19 18:33:33 +0000

    Añadido prueba TDD para individuo, donde hacemos pruebas iniciales para obtener los atributos

---
+++ spec/individuo_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+	RSpec.describe Gema::Individuo do 
+	before: each do 
+	      @mujer1 = Salud.new("Maria",70,1.6,20,0,[80,80.2],[99.3,99.6])
+	      @mujer2 = Salud.new("Raquel",58,1.7,25,0,[51.2,51.5],[70.1,70.3])
+	      @hombre1 = Salud.new("Pablo",60,1.9,35,1,[61.1,61.5],[73.5,73.8])
+	      @hombre2 = Salud.new("Andres",100,1.5,13,1,[110.3,110.6],[99.8,100.2])
+	end
+   
+	describe "Atributos para individuo" do 
+	 it "Existe un nombre para el individuo" do
+           expect(@mujer1).to have_attributes(:nombre=>"Maria")
+	   expect(@hombtre1).to have_attributes(:nombre=>"Pablo")     
+        end
+
+        it "Peso del individuo" do
+            expect(@mujer1).to have_attributes(:peso=>70)
+	    expect(@hombre1).to have_attributes(:peso=>60)
+        end
+
+        it "Talla del individuo " do
+            expect(@mujer2).to have_attributes(:glucidos=>1.7)
+            expect(@hombres2).to have_attributes(:glucidos=>1.5)
+        end
+
+        it "Edad del individuo" do
+            expect(@mujer2).to have_attributes(:edad=>25.0)
+            expect(@hombre2).to have_attributes(:edad=>13.0)
+	end
+    end
+end
