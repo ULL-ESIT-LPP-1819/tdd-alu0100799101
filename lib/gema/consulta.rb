1ddbd922de6c1013c7e3a9433ab88c1cb114a255
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-19 21:18:45 +0000

    Añadimos el codigo fuente con la clase paciente

---
+++ lib/gema/consulta.rb
@@ -0,0 +1,10 @@
+module Gema
+    
+        class Paciente < Individuo
+            
+                def initialize(nombre,peso,talla,edad,sexo,cintura,cadera)
+                
+                    super(nombre,peso,talla,edad,sexo,cintura,cadera)
+                end
+        end
+end
