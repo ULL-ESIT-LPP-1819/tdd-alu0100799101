ce2790bbefd89b9c608cb3733836f334de7b0c84
alu0100799101 <wadrycanario1993@gmail.com>
2018-11-19 19:11:38 +0000

    Añadido el codigo para IMC,grasa y oms

--- lib/gema/individuo.rb
+++ lib/gema/individuo.rb
@@ -3,7 +3,7 @@ module Gema
 	class Individuo
 	attr_reader :nombre,:peso,:talla,:edad ,:sexo
 	
-		def initialize (nombre, peso, talla, edad, sexo, cintura, cadera)
+	    def initialize (nombre, peso, talla, edad, sexo, cintura, cadera)
 		    
 		    @nombre=nombre
 		    @peso = peso
@@ -12,6 +12,29 @@ module Gema
      		@sexo = sexo
      		@cintura = cintura
      		@cadera = cadera
+     		
+		end
+        def imc
+    		@peso/(@talla * @talla)
+    	end
+    	
+    	def grasa
+        	(1.2 * imc) + (0.23 * @edad) - (10.8 * @sexo ) - 5.4 
+    	end
+    	
+    	def oms
+    		case imc
+        	when 0..18.5
+        	"Bajo Peso,Delgado"
+        	when 18.6...24.9
+        	"Adecuado,Aceptable"
+        	when 25..29.9
+        	"Sobrepeso,Sobrepeso"
+        	when 30..34.9
+        	"Obesidad Grado 1,Obesidad"
+        	else
+        	"Obesidad Grado 2,Obesidad"
+    		end
     	end
 	end
 end
