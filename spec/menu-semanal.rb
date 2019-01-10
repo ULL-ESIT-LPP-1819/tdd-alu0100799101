3dbc653f11fc5f92b80e7b34c0650963348d8dbf
alu0100799101 <wadrycanario1993@gmail.com>
2019-01-10 21:21:55 +0000

    Pruebas to_s añadida

--- spec/menu-semanal.rb
+++ spec/menu-semanal.rb
@@ -1,11 +1,11 @@
 RSpec.describe Menu do
   before :each do
 
-    @menu = Menu.new('Lunes') do
+    @menu_semanal = Menu.new('Lunes') do
       titulo    'Bajo en calorías'
       ingesta   min: 30, max: 35
       desayuno  descripcion: 'Pan de trigo integral',
-        porcion: 1,
+        porcion: "1 rodaja",
         gramos: '100g',
         grasas: '3.3g',
         carbohidratos: '54g',
@@ -13,20 +13,20 @@ RSpec.describe Menu do
         fibra: '2.3g',
         sal: '0.06g'
       desayuno  descripcion: 'Actimel',
-        porcion: 1,
+        porcion: " 1 porcion ",
         gramos: '100g',
         grasas: '3.4g',
         carbohidratos: '4.4g',
         proteinas: '3.6g'
       almuerzo  descripcion: 'Arroz',
-        porcion: 1,
+        porcion: " 1 taza",
         gramos: '100g',
         grasas: '0.9g',
         carbohidratos: '4.4g',
         proteinas: '3.6g',
         sal: '0.05g'
       almuerzo  descripcion: 'Lentejas',
-        porcion: 1/2,
+        porcion: "1/2 cucharon",
         gramos: '100g',
         grasas: '0.4g',
         carbohidratos: '20g',
@@ -34,18 +34,27 @@ RSpec.describe Menu do
         fibra: '8g',
         sal: '0.02g'
       almuerzo  descripcion: 'Narajna',
-        porcion: 1,
+        porcion: "1 pieza",
         gramos: '100g',
         grasas: '0.12g',
         carbohidratos: '11.75g',
         proteinas: '0.94g',
         fibra: '0.94g'
       cena      descripcion: 'Leche entera hacendado',
-        porcion: 1,
+        porcion: " 1 vaso ",
         gramos: '100g',
         grasas: '3.6g',
         carbohidratos: '4.6g',
         proteinas: '3.1g'
     end
   end
+    describe "Pruebas DSL" do
+        
+       it "Prueba to_s" do
+           expect(@menu_semanal).to respond_to(:to_s)
+       end
+        
+        
+        
+    end
 end  
